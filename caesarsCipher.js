36e2ac36ca2ec00b3b0e44a686fbd7342445b903
julin <juchuanlin@hotmail.com>
2019-01-04 23:16:17 -0800

    program can read the input argument from the command line, and have readme file

--- caesarsCipher.js
+++ caesarsCipher.js
@@ -1,15 +1,21 @@
 function rot13(str) { // LBH QVQ VG!
+  if (typeof process.argv[2] !== "undefined"){
+    str = process.argv[2];
+  }
+  console.log("encoded str = " + str);
   var baseStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var decodedStr = "";
   var baseStrLength = baseStr.length;
   var shiftCipher = 13;
+
+
   for (var i = 0; i < str.length; i++)
   {
       var pos = baseStr.indexOf(str[i]);
       if (pos === -1){
-          console.log(str[i]);
+          //console.log(str[i]);
           decodedStr += str[i];
-          console.log(decodedStr);
+          //console.log(decodedStr);
       }
       else{//this is a char in the baseStr
         pos += shiftCipher;
@@ -18,10 +24,11 @@ function rot13(str) { // LBH QVQ VG!
             pos -= baseStrLength;
         }
         decodedStr += baseStr.charAt(pos);
-        console.log(decodedStr);
+        //console.log(decodedStr);
       }
   }
-    
+  
+  console.log("decoded Str = " + decodedStr);  
   return decodedStr;
 }
 
---
+++ readme.txt
@@ -0,0 +1,11 @@
+How to run this program?
+1. Clone this program from https://github.com/vvlnote/caesarsCipher.git
+2. in the local machine, node.js needed to be installed.
+3. open the terminal, and go to where this program is located in the local machine
+4. type 
+		node caesarsCipher.js
+	then there is a default encoded string will be decoded
+5. type 
+		node caesarsCipher.js "SERR CVMMN!"
+	then "SERR CVMMN!" will be read in as encoded string, and screen will print out the decoded string
+6. only accept one input encoded string every time run this program in the command line
